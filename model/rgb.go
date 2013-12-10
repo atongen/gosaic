c570adb736923f41ee1908a71a4abf74be3bf3fb
Andrew Tongen <atongen@nerdery.com>
2013-12-09 20:40:06 -0600

    Add documentation comments

---
+++ model/rgb.go
@@ -0,0 +1,8 @@
+// http://www.easyrgb.com/index.php
+// http://en.wikipedia.org/wiki/Color_difference
+// http://en.wikipedia.org/wiki/Dithering
+// http://en.wikipedia.org/wiki/Color_quantization
+
+type RGB struct {
+  R, G, B []int8
+}
